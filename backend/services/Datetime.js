22169ab2b12789b7e32937825857189741a2fa55
qb1968 <mark.allison75@gmail.com>
2024-03-15 13:39:51 -0400

    time

--- backend/services/Datetime.js
+++ backend/services/Datetime.js
@@ -5,8 +5,8 @@ const createDatetime = (date, time) => {
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const day = date.getDate().toString().padStart(2, "0");
-  const datetimeString = `${month}-${day}-${year}T${time}`;
-  const datetime = DateTime.local.fromISO(datetimeString, { zone: timeZone });
+  const datetimeString = `${year}-${month}-${day}T${time}`;
+  const datetime = DateTime.fromISO(datetimeString, { zone: timeZone });
   return datetime.toJSDate();
 };
 
